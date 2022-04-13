4957a4e21f64f7e13b2247677b398c6d3ebc767f
2010shubham-das <102903609+2010shubham-das@users.noreply.github.com>
2022-04-13 16:57:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+//https://teachablemachine.withgoogle.com/models/hkN6hTokV/
+function startClassification() {
+    navigator.mediaDevices.getUserMedia({ audio: true });
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/hkN6hTokV/model.json", modelReady);
+}
+
+function modelReady() {
+    classifier.classify(gotResults);
+}
+
+function gotResults(error, results) {
+    console.log("got result")
+}
